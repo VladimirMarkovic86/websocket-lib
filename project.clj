e2eba33d43ec8d9b4835cb3cdce3377eeadf5f1d
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-08-22 12:15:34 +0200

    Reformatted project.clj

--- project.clj
+++ project.clj
@@ -3,11 +3,7 @@
  :url            "http://gitlab:1610/VladimirMarkovic86/websocket-lib"
  :license        {:name "Eclipse Public License"
                   :url "http://www.eclipse.org/legal/epl-v10.html"}
- :dependencies   [[org.clojure/clojure    "1.9.0"]
-                   ; https://clojure.org/api/api
-                  [org.clojure/clojurescript "1.9.946"]
-                   ; https://clojurescript.org/guides/quick-start#clojurescript-compiler
-                   ; https://clojurescript.org/reference/dependencies
+ :dependencies   [[org.clojure/clojurescript "1.10.339"]
                   ]
  :source-paths ["src/cljs"])
 
